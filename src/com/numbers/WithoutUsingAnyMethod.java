7c8f2e536dc841dcb0e99ac9300e1ab4749c08ce
satywanbhole <satywanbhole@gmail.com>
2022-12-25 12:38:22 +0530

    First Commit: write code for print 100 number

---
+++ src/com/numbers/WithoutUsingAnyMethod.java
@@ -0,0 +1,29 @@
+package com.numbers;
+
+public class WithoutUsingAnyMethod {
+
+	public static void main(String[] args) {
+		print(100);
+		print1(100);
+
+	}
+
+	private static void print(int n) {
+
+		if (n > 0) {
+			print(n - 1);
+			System.out.print(n + "\t");
+		}
+
+	}
+
+	private static void print1(int n) {
+
+		if (n > 0) {
+
+			System.out.println(n + "\t");
+			print1(n - 1);
+		}
+	}
+
+}
