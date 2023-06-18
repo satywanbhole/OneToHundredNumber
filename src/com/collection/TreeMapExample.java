8241228be94eaba4a801e8108709333e61fd3837
satywanbhole <satywanbhole@gmail.com>
2023-06-19 00:17:05 +0530

    collection type code added

---
+++ src/com/collection/TreeMapExample.java
@@ -0,0 +1,19 @@
+package com.collection;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapExample {
+
+	public static void main(String[] args) {
+
+		Map<String, Integer> m=new TreeMap<>();
+		m.put("Amar", 112000);
+		m.put("Pravin", 130000);
+		
+		System.out.println(m);
+		
+
+	}
+
+}
