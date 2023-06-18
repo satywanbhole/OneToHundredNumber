70b77b4a0c1ee8f302cb619960b52532eda4945d
satywanbhole <121335904+satywanbhole@users.noreply.github.com>
2023-06-19 00:23:34 +0530

    Create ListExample.java

---
+++ src/com/collection/ListExample.java
@@ -0,0 +1,16 @@
+public class ListExample {
+
+  // Developer 2
+
+  public static void main(String args[]){
+
+    List<Integer> l=new ArrayList<Integer>();
+    l.add(78);
+    l.add(79);
+    l.add(80);
+
+    System.out.println(l);
+
+    
+  }
+}
