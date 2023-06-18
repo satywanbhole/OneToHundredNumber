6e41a37ed50d4cc03f41130fe1aa76c14c90d5ea
satywanbhole <satywanbhole@gmail.com>
2023-06-18 23:38:51 +0530

    Added collection code

---
+++ src/com/collection/Maps.java
@@ -0,0 +1,51 @@
+package com.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Maps {
+
+	   public static void main(String[] args) {
+		
+		   Map<Integer, String> map=new HashMap<Integer, String>();
+		   
+		   map.put(45, "Amar");
+		   map.put(46, "Pravin");
+		   Set<Integer> ks = map.keySet();
+		   for (Integer i : ks) {
+			   
+			System.out.println(i+" "+map.get(i));
+		}
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+	}
+}
